cf37c2d1031e069642f4d8be2cbb9ed760f54d36
Luís Fernando <luis.moraes@nddigital.com.br>
2014-08-25 09:12:49 -0300

    Corrigindo bug do NullPointerException no AddQuestionFrame.

--- MedindoFebreI/src/UI/AddQuestionJFrame.java
+++ MedindoFebreI/src/UI/AddQuestionJFrame.java
@@ -208,6 +208,7 @@ public class AddQuestionJFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        if(cmbDisciplina.getItemCount() <= 0) return;
         Disciplina d = (Disciplina) cmbDisciplina.getSelectedItem();
         loadMaterias(d.getId());
     }//GEN-LAST:event_formWindowOpened
